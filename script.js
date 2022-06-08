7d192dd4d15dcc6bcf82cd8072f21241b404ac94
Lucas Tavares <l.tavaresc@hotmail.com>
2022-06-08 17:54:05 -0300

    Layout pronto e inicio do js

---
+++ script.js
@@ -0,0 +1,6 @@
+function pedido(id, valor) {
+    let card = '.food-card'
+    // card.style.border = "thick solid #32B72F";
+    document.querySelector('#' + id).style.border = "thick solid #32B72F";
+    // alert(id +' ' + valor)
+}
